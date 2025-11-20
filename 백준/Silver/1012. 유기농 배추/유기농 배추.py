d28afff0b88153b178f861eb11acc3cc61fe65ba
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2025-11-20 16:02:17 +0900

    [Silver II] Title: 유기농 배추, Time: 228 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -8,23 +8,25 @@ for _ in range(T):
     M,N,K= map(int,input().split())
     board = [[0]*M for _ in range(N)]
     visited = [[False]*M for _ in range(N)]
+    target=[]
     cnt=0
     queue = deque()
     for p in range(K):
         i,j =map(int,input().split())
         board[j][i] = 1
+        target.append((j,i))
+        
     
-    for i in range(N):
-        for j in range(M):
-            if not visited[i][j] and board[i][j]==1:
-                cnt+=1
-                queue.append((i,j))
-                visited[i][j] = True
-                while queue:
-                    bi,bj = queue.popleft()
-                    for wi,wj in move:
-                        ci ,cj = bi+wi,bj+wj
-                        if 0<=ci<N and 0<=cj<M and not visited[ci][cj] and board[ci][cj]==1:
-                            queue.append((ci,cj))
-                            visited[ci][cj]=True
+    for i,j in target:
+        if not visited[i][j] and board[i][j]==1:
+            cnt+=1
+            queue.append((i,j))
+            visited[i][j] = True
+            while queue:
+                bi,bj = queue.popleft()
+                for wi,wj in move:
+                    ci ,cj = bi+wi,bj+wj
+                    if 0<=ci<N and 0<=cj<M and not visited[ci][cj] and board[ci][cj]==1:
+                        queue.append((ci,cj))
+                        visited[ci][cj]=True
     print(cnt)
