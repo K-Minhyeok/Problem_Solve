0f2a86ad528f81a20b82954ffe2da4d8c32c969a
Min_h_bot <74346735+K-Minhyeok@users.noreply.github.com>
2025-11-29 20:18:28 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+def backtracking(i):
+    if len(res) == M:
+        print(*res)
+        return
+    
+    for i in range(i,N+1):
+            res.append(i)
+            backtracking(i)
+            res.pop()
+
+
+
+N,M = map(int,input().split())
+res = []
+
+backtracking(1)
